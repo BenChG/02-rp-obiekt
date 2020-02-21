0b2bb6d9a6c30d8f8055e2a8ffbe67c2cbc34c25
Benedykt Chalubinski-Gonerko <ben.chalubinski@gmail.com>
2020-02-21 05:56:12 +0100

    Created class person & added feature add new person

---
+++ Osoba.cpp
@@ -0,0 +1,60 @@
+#include "Osoba.h"
+
+
+void Osoba::ustawId(int noweId)
+{
+    if (noweId >= 0)
+        id = noweId;
+}
+void Osoba::ustawImie (string noweImie)
+{
+    imie = noweImie;
+}
+void Osoba::ustawNazwisko (string noweNazwisko)
+{
+    nazwisko = noweNazwisko;
+}
+
+void Osoba::ustawNumerTelefonu (string nowyNumerTelefonu)
+{
+    numerTelefonu = nowyNumerTelefonu;
+}
+
+void Osoba::ustawEmail (string nowyEmail)
+{
+    email = nowyEmail;
+}
+
+void Osoba::ustawAdres(string nowyAdres)
+{
+    adres = nowyAdres;
+}
+
+int Osoba::pobierzId()
+{
+    return id;
+}
+string Osoba::pobierzImie()
+{
+    return imie;
+}
+
+string Osoba::pobierzNazwisko()
+{
+    return nazwisko;
+}
+
+string Osoba::pobierzNumerTelefonu()
+{
+    return numerTelefonu;
+}
+
+string Osoba::pobierzEmail()
+{
+    return email;
+}
+
+string Osoba::pobierzAdres()
+{
+    return adres;
+}
